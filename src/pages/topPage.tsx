81de9e39c11b0ba63a61fd51c5bc39dddfca8f28
HIBIKIN <hi3bi1ki5@gmail.com>
2020-05-22 23:52:24 +0900

    余白のレスポンシブ 対応

--- src/pages/topPage.tsx
+++ src/pages/topPage.tsx
@@ -10,26 +10,60 @@ import Header from '../components/header'
 import Footer from '../components/footer';
 import StartBlock from '../components/startBlock';
 
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyle = makeStyles(() =>
+  createStyles({
+    root: {
+      height: '100vh',
+    },
+    blank: {
+      backgroundColor: '#F8E266',
+    },
+
+  }),
+)
 
 
 const TopPage: FC = () => {
+  const classes = useStyle();
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Header />
-            <StartBlock />
-          <Footer />
+          <Grid container className={classes.root}>
+            <Grid item sm={3} className={classes.blank}></Grid>
+            <Grid item sm={6} xs={12}>
+              <Header />
+              <StartBlock />
+              <Footer />
+            </Grid>
+            <Grid item sm={3} className={classes.blank}></Grid>
+          </Grid>
         </Route>
         <Route exact path="/question">
-          <Header />
-          <QuestionPage />
-          <Footer />
+          <Grid container className={classes.root}>
+            <Grid item sm={3} className={classes.blank}></Grid>
+            <Grid item sm={6} xs={12}>
+              <Header />
+              <QuestionPage />
+              <Footer />
+            </Grid>
+            <Grid item sm={3} className={classes.blank}></Grid>
+          </Grid>
         </Route>
         <Route exact path="/result">
-          <Header />
-          <ResultPage />
-          <Footer />
+          <Grid container className={classes.root}>
+            <Grid item sm={3} className={classes.blank}></Grid>
+            <Grid item sm={6} xs={12}>
+              <Header />
+              <ResultPage />
+              <Footer />
+            </Grid>
+            <Grid item sm={3} className={classes.blank}></Grid>
+          </Grid>
         </Route>
       </Switch>
     </Router>
